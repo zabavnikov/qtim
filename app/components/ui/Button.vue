a8276b15174347420a4eecd327efddbbcc00c713
Denis <zabavnikov.com@gmail.com>
2025-07-31 09:11:59 +0700

    wip

--- app/components/ui/Button.vue
+++ app/components/ui/Button.vue
@@ -1,25 +1,13 @@
 <script setup lang="ts">
-import { computed } from 'vue'
-
 export interface Button {
 	type?: 'submit' | 'button' | 'reset'
-	variant?: 'primary'
 }
 
-const { type = 'button', variant = 'primary' } = defineProps<Button>()
-
-/*
-	Вынес в вычисляемое свойство,
-	чтобы в дальнейшем можно было применять
- 	различные variant для кнопки.
- */
-const classes = computed(() => {
-	return ['h-[52px] rounded-full bg-black px-8 text-white']
-})
+const { type = 'button' } = defineProps<Button>()
 </script>
 
 <template>
-	<button :type :class="classes">
+	<button :type="type" class="'h-[52px] rounded-full bg-black px-8 text-white">
 		<slot />
 	</button>
 </template>
