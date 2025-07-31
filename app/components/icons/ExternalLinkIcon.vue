bd4c2c57d4133b9732a5efdb042274b206ba2f92
Denis <zabavnikov.com@gmail.com>
2025-07-31 09:09:52 +0700

    wip

---
+++ app/components/icons/ExternalLinkIcon.vue
@@ -0,0 +1,18 @@
+<template>
+	<svg
+		width="32"
+		height="32"
+		viewBox="0 0 32 32"
+		fill="none"
+		xmlns="http://www.w3.org/2000/svg"
+	>
+		<rect width="32" height="32" rx="16" fill="#101010" />
+		<path
+			d="M17.5 20.5L20.5 11.5L11.5 14.5L15.25 16.75L17.5 20.5Z"
+			stroke="#E2BEFF"
+			stroke-width="1.4"
+			stroke-linecap="round"
+			stroke-linejoin="round"
+		/>
+	</svg>
+</template>
--- app/components/layout/LayoutFooter.vue
+++ app/components/layout/LayoutFooter.vue
@@ -1,18 +1,58 @@
 <script setup lang="ts">
 import Logotype from '~/components/ui/Logotype.vue'
 import LayoutHeaderNav from '~/components/layout/LayoutHeaderNav.vue'
+import ExternalLinkIcon from "~/components/icons/ExternalLinkIcon.vue";
 </script>
 
 <template>
-	<footer class="bg-pink pt-[140px]">
+	<footer class="bg-pink pt-[140px] overflow-hidden">
 		<div class="container">
-			<div class="grid grid-cols-2">
-				<div>
-					<p class="mb-16">Is there a project?</p>
-					<p class="text-hero">Let’s work<br />together</p>
+			<p class="mb-16">Is there a project?</p>
+
+			<div class="grid grid-cols-3">
+				<div class="col-span-2">
+					<p class="text-hero underline">Let’s work<br />together</p>
+				</div>
+				<div class="space-y-8">
+					<div>
+            <p class="mb-3">For all questions</p>
+            <p><a href="tel:+79202780068">+7 (920) 278—00—68</a></p>
+            <p><a href="mailto:qtim@agency.com">qtim@agency.com</a></p>
+          </div>
+          <div>
+            <p class="mb-3">Office in Russia</p>
+            <p class="mb-2">139 Ruben Station Lake<br />Drew, NM 52678</p>
+            <ExternalLinkIcon />
+          </div>
+          <div>
+            <p class="mb-3">Office in Dubai</p>
+            <p class="mb-2">139 Ruben Station Lake<br />Drew, NM 52678</p>
+            <ExternalLinkIcon />
+          </div>
 				</div>
 			</div>
-			<div class="h-[140px]"></div>
+
+      <div class="h-[140px]"></div>
+
+      <div class="grid grid-cols-12">
+        <div class="col-span-4 flex items-end pb-[30px]">
+          <ul class="flex items-center gap-8 underline underline-offset-6">
+            <li>Facebook</li>
+            <li>Instagram</li>
+            <li>Telegram</li>
+          </ul>
+        </div>
+        <div class="col-span-4 flex justify-center">
+          <LayoutFooterCTAButton />
+        </div>
+        <div class="col-span-4 flex items-end justify-end pb-[30px]">
+          <ul class="flex items-center gap-8 underline underline-offset-6">
+            <li>Behance</li>
+            <li>Linkedn</li>
+            <li>Privacy Policy</li>
+          </ul>
+        </div>
+      </div>
 		</div>
 	</footer>
 </template>
